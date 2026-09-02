6447ca10ed284bd785ddbf82fcebf31567f8355f
eng-xitao <eng.wsouza@gmail.com>
2026-09-02 18:01:55 -0300

    feat: ativar padrao global responsivo e impressao

--- app/src/main.jsx
+++ app/src/main.jsx
@@ -1,5 +1,7 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import './theme.css'
+import './standard-responsive-print.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
